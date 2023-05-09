19040ee3bee793aeece09b9c86253081cea79303
taeminHan <taemin9705@gmail.com>
2023-05-10 00:02:04 +0900

    [EUREKA]Eureka-server 추가
    - Eureka Survey 및 환경 세팅
    - 개발 서버 업로드 및 반영 완료

---
+++ mpe-eureka/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.mpe"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+// https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-netflix-eureka-server
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server:4.0.1")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
